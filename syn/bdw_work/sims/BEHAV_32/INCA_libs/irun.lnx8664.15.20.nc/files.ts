d36bf38a2a0ca8065fd00826e0267f9c808d3578
yl4337 <yl4337@columbia.edu>
2020-03-18 15:25:23 -0400

    delete bdw-work

--- syn/bdw_work/sims/BEHAV_32/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++
@@ -1,9 +0,0 @@
-1583445398 /home/esp2020/yl4337/SHA/SystemC/Embedded_Scalable_Platform/syn/bdw_work/modules/sha/IOCFG_32/incisive/sha.o
-1583445406 /home/esp2020/yl4337/SHA/SystemC/Embedded_Scalable_Platform/syn/bdw_work/modules/tb/TESTBENCH_32/incisive/driver.o
-1583445417 /home/esp2020/yl4337/SHA/SystemC/Embedded_Scalable_Platform/syn/bdw_work/modules/tb/TESTBENCH_32/incisive/memory.o
-1583445425 /home/esp2020/yl4337/SHA/SystemC/Embedded_Scalable_Platform/syn/bdw_work/modules/tb/TESTBENCH_32/incisive/system.o
-1583445447 /home/esp2020/yl4337/SHA/SystemC/Embedded_Scalable_Platform/syn/bdw_work/modules/tb/TESTBENCH_32/incisive/sc_main.o
-1583445473 /home/esp2020/yl4337/SHA/SystemC/Embedded_Scalable_Platform/syn/bdw_work/libesc/incisive/libesc.a
-1535408329 /opt/cadence/stratus1815/tools.lnx86/stratus/lib/64bit/libhubexec.so
-1535408329 /opt/cadence/stratus1815/tools.lnx86/stratus/lib/64bit/libhub.so
-1535408330 /opt/cadence/stratus1815/tools.lnx86/stratus/lib/64bit/libbdw_st.so
--- syn/bdw_work/sims/BEHAV_32/INCA_libs/irun.lnx8664.15.20.nc/ncsc_run/ncsc_obj/ncsc_nc_main_object.d
+++
@@ -1,1 +0,0 @@
-main.o: /opt/cad/incisive/tools/inca/src/main.cc
